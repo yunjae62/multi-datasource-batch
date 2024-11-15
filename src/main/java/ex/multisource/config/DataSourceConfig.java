66ef90e9c27eba6bf3a423c0aa55c5073495ece9
yunjae62 <wodbs7758@naver.com>
2024-11-15 23:39:31 +0900

    feat: 배치 메타데이터 테이블 유무에 따른 스크립트 실행 설정

--- src/main/java/ex/multisource/config/DataSourceConfig.java
+++ src/main/java/ex/multisource/config/DataSourceConfig.java
@@ -2,16 +2,24 @@ package ex.multisource.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
+@RequiredArgsConstructor
 public class DataSourceConfig {
+
+    private final ResourceLoader resourceLoader;
 
     @Bean
     @Primary
@@ -34,5 +42,32 @@ public class DataSourceConfig {
     @ConfigurationProperties(prefix = "spring.datasource.hikari.sub")
     public DataSource subDataSource() {
         return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public DataSourceInitializer batchDataSourceInitializer(@Qualifier("subDataSourceProxy") DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        if (isBatchTablesPresent(jdbcTemplate)) {
+            return null;
+        }
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(resourceLoader.getResource("classpath:org/springframework/batch/core/schema-postgresql.sql"));
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
+    }
+
+    private boolean isBatchTablesPresent(JdbcTemplate jdbcTemplate) {
+        try {
+            jdbcTemplate.execute("SELECT 1 FROM BATCH_JOB_INSTANCE LIMIT 1");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
