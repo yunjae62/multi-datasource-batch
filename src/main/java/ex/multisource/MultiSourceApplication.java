e2b1fd49d495d8078265553e34020c1ed42cfeac
yunjae62 <wodbs7758@naver.com>
2024-11-15 13:39:14 +0900

    first commit

---
+++ src/main/java/ex/multisource/MultiSourceApplication.java
@@ -0,0 +1,12 @@
+package ex.multisource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSourceApplication.class, args);
+    }
+}
---
+++ src/test/java/ex/multisource/MultiSourceApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.multisource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
