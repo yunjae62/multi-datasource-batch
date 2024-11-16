fd3f6c6dbfd66718066b0c5d06b25062ebf456c9
yunjae62 <wodbs7758@naver.com>
2024-11-16 13:25:08 +0900

    feat: readOnly에 따른 라우팅 데이터소스 추가

---
+++ src/main/java/ex/multisource/config/readwrite/TransactionRoutingDataSource.java
@@ -0,0 +1,14 @@
+package ex.multisource.config.readwrite;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class TransactionRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+            ? DataSourceType.READ_ONLY
+            : DataSourceType.READ_WRITE;
+    }
+}
