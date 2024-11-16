63de93771011a183c834c3d0045c8501ab534d4c
yunjae62 <wodbs7758@naver.com>
2024-11-16 13:26:35 +0900

    feat: 상품 단건 조회 API 추가

--- src/main/java/ex/multisource/controller/ProductController.java
+++ src/main/java/ex/multisource/controller/ProductController.java
@@ -6,6 +6,8 @@ import ex.multisource.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Product product = productService.getProduct(id);
+        return ResponseEntity.ok(product);
+    }
 
     @PostMapping
     public ResponseEntity<Product> saveProduct(@RequestBody ProductCreateReq request) {
