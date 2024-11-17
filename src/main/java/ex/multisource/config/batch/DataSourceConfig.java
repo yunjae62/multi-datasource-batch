105d46dc1f839802a621dd4f7668f42e366d0647
yunjae62 <wodbs7758@naver.com>
2024-11-17 12:46:07 +0900

    feat: batch 분리 주석 해제

--- src/main/java/ex/multisource/config/batch/DataSourceConfig.java
+++ src/main/java/ex/multisource/config/batch/DataSourceConfig.java
@@ -1,73 +1,73 @@
-//package ex.multisource.config;
-//
-//import com.zaxxer.hikari.HikariDataSource;
-//import javax.sql.DataSource;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.core.io.ResourceLoader;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-//import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-//import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class DataSourceConfig {
-//
-//    private final ResourceLoader resourceLoader;
-//
-//    @Bean
-//    @Primary
-//    public DataSource mainDataSourceProxy(@Qualifier("mainDataSource") DataSource mainDataSource) {
-//        return new LazyConnectionDataSourceProxy(mainDataSource);
-//    }
-//
-//    @Bean
-//    public DataSource subDataSourceProxy(@Qualifier("subDataSource") DataSource subDataSource) {
-//        return new LazyConnectionDataSourceProxy(subDataSource);
-//    }
-//
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource.hikari.main")
-//    public DataSource mainDataSource() {
-//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-//    }
-//
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource.hikari.sub")
-//    public DataSource subDataSource() {
-//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-//    }
-//
-//    @Bean
-//    public DataSourceInitializer batchDataSourceInitializer(@Qualifier("subDataSourceProxy") DataSource dataSource) {
-//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-//
-//        if (isBatchTablesPresent(jdbcTemplate)) {
-//            return null;
-//        }
-//
-//        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-//        populator.addScript(resourceLoader.getResource("classpath:org/springframework/batch/core/schema-postgresql.sql"));
-//
-//        DataSourceInitializer initializer = new DataSourceInitializer();
-//        initializer.setDataSource(dataSource);
-//        initializer.setDatabasePopulator(populator);
-//
-//        return initializer;
-//    }
-//
-//    private boolean isBatchTablesPresent(JdbcTemplate jdbcTemplate) {
-//        try {
-//            jdbcTemplate.execute("SELECT 1 FROM BATCH_JOB_INSTANCE LIMIT 1");
-//            return true;
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-//}
+package ex.multisource.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfig {
+
+    private final ResourceLoader resourceLoader;
+
+    @Bean
+    @Primary
+    public DataSource mainDataSourceProxy(@Qualifier("mainDataSource") DataSource mainDataSource) {
+        return new LazyConnectionDataSourceProxy(mainDataSource);
+    }
+
+    @Bean
+    public DataSource subDataSourceProxy(@Qualifier("subDataSource") DataSource subDataSource) {
+        return new LazyConnectionDataSourceProxy(subDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari.main")
+    public DataSource mainDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari.sub")
+    public DataSource subDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public DataSourceInitializer batchDataSourceInitializer(@Qualifier("subDataSourceProxy") DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        if (isBatchTablesPresent(jdbcTemplate)) {
+            return null;
+        }
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(resourceLoader.getResource("classpath:org/springframework/batch/core/schema-postgresql.sql"));
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
+    }
+
+    private boolean isBatchTablesPresent(JdbcTemplate jdbcTemplate) {
+        try {
+            jdbcTemplate.execute("SELECT 1 FROM BATCH_JOB_INSTANCE LIMIT 1");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
